821835faf38b61fec7ccda1e247e25088b0a2597
Kirill Rakhman <kirill.rakhman@gmail.com>
2025-02-10 17:22:30 +0100

    Fix RunSelectedFilesSpecificTestsAction

--- src/org/jetbrains/kotlin/test/helper/actions/RunSelectedFilesTestsAction.kt
+++ src/org/jetbrains/kotlin/test/helper/actions/RunSelectedFilesTestsAction.kt
@@ -4,14 +4,19 @@ import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.application.ModalityState
+import com.intellij.openapi.application.ReadAction
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.ui.popup.JBPopupFactory
 import com.intellij.psi.PsiClass
 import com.intellij.psi.util.parentsOfType
+import com.intellij.util.concurrency.AppExecutorUtil
 import org.jetbrains.kotlin.test.helper.TestDataPathsConfiguration
 import org.jetbrains.kotlin.test.helper.buildRunnerLabel
 import org.jetbrains.kotlin.test.helper.isTestDataFile
 import org.jetbrains.kotlin.test.helper.runGradleCommandLine
+import java.util.concurrent.Callable
+import javax.swing.ListSelectionModel
 
 abstract class RunSelectedFilesActionBase : AnAction(), DumbAware {
     override fun update(e: AnActionEvent) {
@@ -38,21 +43,35 @@ class RunSelectedFilesTestsAction : RunSelectedFilesActionBase() {
 class RunSelectedFilesSpecificTestsAction : RunSelectedFilesActionBase() {
     override fun actionPerformed(e: AnActionEvent) {
         val project = e.project ?: return
-        val selectedFiles = e.getData(CommonDataKeys.VIRTUAL_FILE_ARRAY)?.filter { it.isTestDataFile(project) } ?: return
-        val testMethods = selectedFiles.flatMap { it.collectTestMethods(project) }
-        val byClass = testMethods.groupBy { it.parentsOfType<PsiClass>().last() }
 
-        val testTags = TestDataPathsConfiguration.getInstance(project).testTags
+        ReadAction
+            .nonBlocking(Callable {
+                val selectedFiles = e.getData(CommonDataKeys.VIRTUAL_FILE_ARRAY)
+                    ?.filter { it.isTestDataFile(project) }
+                    ?: return@Callable emptyMap()
 
-        val popup = JBPopupFactory.getInstance()
-            .createPopupChooserBuilder(byClass.keys.map { it.buildRunnerLabel(testTags) }.sortedBy { it })
-            .setTitle("Select Test Class")
-            .setItemChosenCallback { selected ->
-                val commandLine = computeGradleCommandLine(byClass.entries.first { it.key.name == selected }.value)
-                runGradleCommandLine(e, commandLine)
-            }
-            .createPopup()
+                val testTags = TestDataPathsConfiguration.getInstance(project).testTags
+                selectedFiles
+                    .flatMap { it.collectTestMethods(project) }
+                    .groupBy { it.parentsOfType<PsiClass>().last() }
+                    .mapKeys { it.key.buildRunnerLabel(testTags) }
+            })
+            .inSmartMode(project)
+            .finishOnUiThread(ModalityState.defaultModalityState(), { byClass ->
+                if (byClass.isEmpty()) return@finishOnUiThread
 
-        popup.showInBestPositionFor(e.dataContext)
+                JBPopupFactory.getInstance()
+                    .createPopupChooserBuilder(byClass.keys.sortedBy { it })
+                    .setTitle("Select Test Class")
+                    .setItemChosenCallback { selected ->
+                        val commandLine = computeGradleCommandLine(byClass[selected]!!)
+                        runGradleCommandLine(e, commandLine)
+                    }
+                    .setNamerForFiltering { it }
+                    .setSelectionMode(ListSelectionModel.SINGLE_SELECTION)
+                    .createPopup()
+                    .showInBestPositionFor(e.dataContext)
+            })
+            .submit(AppExecutorUtil.getAppExecutorService())
     }
 }
