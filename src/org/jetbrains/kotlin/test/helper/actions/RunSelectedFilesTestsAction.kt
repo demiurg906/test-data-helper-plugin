e59503ec78d7adbdce77c24690dc568056e89ddc
Kirill Rakhman <kirill.rakhman@gmail.com>
2025-02-11 10:47:16 +0100

    Rewrite actions using service and coroutines

--- src/org/jetbrains/kotlin/test/helper/actions/RunSelectedFilesTestsAction.kt
+++ src/org/jetbrains/kotlin/test/helper/actions/RunSelectedFilesTestsAction.kt
@@ -6,19 +6,24 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.application.ModalityState
 import com.intellij.openapi.application.ReadAction
+import com.intellij.openapi.components.service
 import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.popup.JBPopupFactory
+import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiClass
+import com.intellij.psi.PsiMethod
 import com.intellij.psi.util.parentsOfType
 import com.intellij.util.concurrency.AppExecutorUtil
 import org.jetbrains.kotlin.test.helper.TestDataPathsConfiguration
 import org.jetbrains.kotlin.test.helper.buildRunnerLabel
 import org.jetbrains.kotlin.test.helper.isTestDataFile
 import org.jetbrains.kotlin.test.helper.runGradleCommandLine
+import org.jetbrains.kotlin.test.helper.services.TestDataRunnerService
 import java.util.concurrent.Callable
 import javax.swing.ListSelectionModel
 
-abstract class RunSelectedFilesActionBase : AnAction(), DumbAware {
+abstract class RunSelectedFilesActionBase : AnAction() {
     override fun update(e: AnActionEvent) {
         val project = e.project ?: return
         val selectedFiles = e.getData(CommonDataKeys.VIRTUAL_FILE_ARRAY) ?: emptyArray()
@@ -33,45 +38,15 @@ abstract class RunSelectedFilesActionBase : AnAction(), DumbAware {
 class RunSelectedFilesTestsAction : RunSelectedFilesActionBase() {
     override fun actionPerformed(e: AnActionEvent) {
         val project = e.project ?: return
-        val selectedFiles = e.getData(CommonDataKeys.VIRTUAL_FILE_ARRAY)?.filter { it.isTestDataFile(project) } ?: return
-        val testMethods = selectedFiles.flatMap { it.collectTestMethods(project) }
-        val commandLine = computeGradleCommandLine(testMethods)
-        runGradleCommandLine(e, commandLine)
+        project.service<TestDataRunnerService>()
+            .collectAndRunAllTests(e, e.getData(CommonDataKeys.VIRTUAL_FILE_ARRAY)?.toList())
     }
 }
 
 class RunSelectedFilesSpecificTestsAction : RunSelectedFilesActionBase() {
     override fun actionPerformed(e: AnActionEvent) {
         val project = e.project ?: return
-
-        ReadAction
-            .nonBlocking(Callable {
-                val selectedFiles = e.getData(CommonDataKeys.VIRTUAL_FILE_ARRAY)
-                    ?.filter { it.isTestDataFile(project) }
-                    ?: return@Callable emptyMap()
-
-                val testTags = TestDataPathsConfiguration.getInstance(project).testTags
-                selectedFiles
-                    .flatMap { it.collectTestMethods(project) }
-                    .groupBy { it.parentsOfType<PsiClass>().last() }
-                    .mapKeys { it.key.buildRunnerLabel(testTags) }
-            })
-            .inSmartMode(project)
-            .finishOnUiThread(ModalityState.defaultModalityState(), { byClass ->
-                if (byClass.isEmpty()) return@finishOnUiThread
-
-                JBPopupFactory.getInstance()
-                    .createPopupChooserBuilder(byClass.keys.sortedBy { it })
-                    .setTitle("Select Test Class")
-                    .setItemChosenCallback { selected ->
-                        val commandLine = computeGradleCommandLine(byClass[selected]!!)
-                        runGradleCommandLine(e, commandLine)
-                    }
-                    .setNamerForFiltering { it }
-                    .setSelectionMode(ListSelectionModel.SINGLE_SELECTION)
-                    .createPopup()
-                    .showInBestPositionFor(e.dataContext)
-            })
-            .submit(AppExecutorUtil.getAppExecutorService())
+        project.service<TestDataRunnerService>()
+            .collectAndRunSpecificTests(e, e.getData(CommonDataKeys.VIRTUAL_FILE_ARRAY)?.toList())
     }
 }
---
+++ src/org/jetbrains/kotlin/test/helper/services/TestDataRunnerService.kt
@@ -0,0 +1,101 @@
+package org.jetbrains.kotlin.test.helper.services
+
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.application.EDT
+import com.intellij.openapi.application.smartReadAction
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.popup.JBPopupFactory
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.platform.ide.progress.withBackgroundProgress
+import com.intellij.platform.util.progress.reportSequentialProgress
+import com.intellij.platform.util.progress.withProgressText
+import com.intellij.psi.PsiClass
+import com.intellij.psi.PsiMethod
+import com.intellij.psi.util.parentsOfType
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.jetbrains.kotlin.test.helper.TestDataPathsConfiguration
+import org.jetbrains.kotlin.test.helper.actions.collectTestMethods
+import org.jetbrains.kotlin.test.helper.actions.computeGradleCommandLine
+import org.jetbrains.kotlin.test.helper.buildRunnerLabel
+import org.jetbrains.kotlin.test.helper.isTestDataFile
+import org.jetbrains.kotlin.test.helper.runGradleCommandLine
+import javax.swing.ListSelectionModel
+
+@Service(Service.Level.PROJECT)
+class TestDataRunnerService(
+    val project: Project,
+    val scope: CoroutineScope
+) {
+    fun collectAndRunAllTests(e: AnActionEvent, files: List<VirtualFile>?) {
+        if (files == null) return
+
+        scope.launch(Dispatchers.Default) {
+            val commandLine = withBackgroundProgress(project, title = "Running all tests") {
+                reportSequentialProgress { reporter ->
+                    reporter.indeterminateStep("Collecting tests")
+
+                    smartReadAction(project) {
+                        val testMethods = filterAndCollectTestMethods(files)
+                        computeGradleCommandLine(testMethods)
+                    }
+                }
+            }
+
+            runGradleCommandLine(e, commandLine)
+        }
+    }
+
+    fun collectAndRunSpecificTests(e: AnActionEvent, files: List<VirtualFile>?) {
+        if (files == null) return
+
+        scope.launch(Dispatchers.Default) {
+            val byClass = withBackgroundProgress(project, title = "Running specific tests") {
+                reportSequentialProgress { reporter ->
+                    reporter.indeterminateStep("Collecting tests")
+
+                    smartReadAction(project) {
+                        val testMethods = filterAndCollectTestMethods(files)
+                        val testTags = TestDataPathsConfiguration.getInstance(project).testTags
+                        testMethods
+                            .groupBy { it.parentsOfType<PsiClass>().last() }
+                            .mapKeys { it.key.buildRunnerLabel(testTags) }
+                    }
+                }
+            }
+
+            withContext(Dispatchers.EDT) {
+                JBPopupFactory.getInstance()
+                    .createPopupChooserBuilder(byClass.keys.sortedBy { it })
+                    .setTitle("Select Test Class")
+                    .setItemChosenCallback { selected ->
+                        val testMethods = byClass[selected] ?: return@setItemChosenCallback
+
+                        scope.launch(Dispatchers.Default) {
+                            val commandLine = smartReadAction(project) {
+                                computeGradleCommandLine(testMethods)
+                            }
+
+                            withContext(Dispatchers.EDT) {
+                                runGradleCommandLine(e, commandLine)
+                            }
+                        }
+
+                    }
+                    .setNamerForFiltering { it }
+                    .setSelectionMode(ListSelectionModel.SINGLE_SELECTION)
+                    .createPopup()
+                    .showInBestPositionFor(e.dataContext)
+            }
+        }
+    }
+
+    fun filterAndCollectTestMethods(files: List<VirtualFile>?): List<PsiMethod> {
+        if (files == null) return emptyList()
+        return files.filter { it.isTestDataFile(project) }.flatMap { it.collectTestMethods(project) }
+    }
+}
