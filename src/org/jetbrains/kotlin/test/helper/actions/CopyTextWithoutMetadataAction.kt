99d7a3b33894550e07d9176d7b5e0908c9979296
Alexey Belkov <alexey.belkov@jetbrains.com>
2025-02-18 16:24:51 +0400

    Add action to copy testdata text without metadata
    Fixes #50

---
+++ src/org/jetbrains/kotlin/test/helper/actions/CopyTextWithoutMetadataAction.kt
@@ -0,0 +1,39 @@
+package org.jetbrains.kotlin.test.helper.actions
+
+import com.intellij.openapi.actionSystem.ActionUpdateThread
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.ide.CopyPasteManager
+import java.awt.datatransfer.StringSelection
+
+/**
+ * Copies the selected text into the clipboard without common Kotlin testdata metadata (diagnostics, <caret>, etc.).
+ * A custom shortcut can be assigned for the "Copy Without Metadata" action in the IDE settings.
+ */
+class CopyTextWithoutMetadataAction : AnAction() {
+    override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
+
+    override fun update(e: AnActionEvent) {
+        val editor = e.getData(CommonDataKeys.EDITOR)
+        val isEnabled = editor?.selectionModel?.hasSelection() == true
+        e.presentation.setEnabled(isEnabled)
+    }
+
+    override fun actionPerformed(e: AnActionEvent) {
+        val editor = e.getData(CommonDataKeys.EDITOR) ?: return
+        val selectedText = editor.selectionModel.selectedText
+        if (selectedText.isNullOrBlank()) return
+        val strippedText = selectedText.replace(allMetadataRegex, "")
+        CopyPasteManager.getInstance().setContents(StringSelection(strippedText))
+    }
+}
+
+// See org.jetbrains.kotlin.codeMetaInfo.CodeMetaInfoParser
+private val openingDiagnosticRegex = """(<!([^"]*?((".*?")(, ".*?")*?)?[^"]*?)!>)""".toRegex()
+private val closingDiagnosticRegex = """(<!>)""".toRegex()
+
+private val xmlLikeTagsRegex = """(</?(?:selection|expr|caret)>)""".toRegex()
+
+private val allMetadataRegex =
+    """(${closingDiagnosticRegex.pattern}|${openingDiagnosticRegex.pattern}|${xmlLikeTagsRegex.pattern})""".toRegex()
