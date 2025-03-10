4d32e92535722501c277c0ece23fa49626dcaea6
Ivan Kochurkin <ivan.kochurkin@jetbrains.com>
2025-03-10 17:55:28 +0100

    Add tests for `asPathWithoutAllExtensions`

---
+++ test/org.jetbrains.kotlin.test.helper/Tests.kt
@@ -0,0 +1,22 @@
+import org.jetbrains.kotlin.test.helper.asPathWithoutAllExtensions
+import java.io.File
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Tests {
+    companion object {
+        private val s = File.separatorChar
+    }
+
+    @Test
+    fun testAsPathWithoutAllExtensions() {
+        assertEquals("", "".asPathWithoutAllExtensions)
+        assertEquals("example", "example.kt".asPathWithoutAllExtensions)
+        assertEquals("example", "example.fir.kt".asPathWithoutAllExtensions)
+        assertEquals("path${s}example", "path${s}example.fir.kt".asPathWithoutAllExtensions)
+        assertEquals("path${s}to${s}example", "path${s}to${s}example.fir.kt".asPathWithoutAllExtensions)
+
+        // Not sure why digit-based extensions are not considered as real extensions, but keep the existing behavior for now
+        assertEquals("path${s}example.test.123", "path${s}example.test.123.kt".asPathWithoutAllExtensions)
+    }
+}
